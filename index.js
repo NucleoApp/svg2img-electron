8a7517ad1cc1422ff830d163c1ea8102432a03f1
Dmytry <sharpfuryz@gmail.com>
2017-06-02 12:33:09 +0300

    Feature: reuse window

--- index.js
+++ index.js
@@ -3,53 +3,60 @@
  * https://github.com/sharpfuryz/svg2img-electron
  * Licensed under the MIT license.
  */
-const url = require('url');
-const electron = require('electron');
-const ipcMain = electron.ipcMain;
-var BrowserWindow = electron.BrowserWindow;// || electron.remote.BrowserWindow;
 /*global require:true*/
 /*global __dirname:true*/
 /*global console:true*/
+var win = null;
 var svg2imgElectron = function (svg, options) {
+    const url = require('url');
+    const electron = require('electron');
+    const ipcMain = electron.ipcMain;
+    const os = require('os');
+    const fs = require('fs');
+    const path = require('path');
+    const BrowserWindow = electron.BrowserWindow;
+
     return new Promise(function (resolve) {
-        var os = require('os');
-        var fs = require('fs');
-        var path = require('path');
 
         "use strict";
 
         var electronProcess = function (code, options) {
-            var win = new BrowserWindow({
-                x: 0,
-                y: 0,
-                width: options.width,
-                height: options.height,
-                show: false,
-                frame: false,
-                enableLargerThanScreen: true
-            });
-            win.once('closed', function () {
-                win = null;
-            });
-            win.loadURL(url.format({
-                pathname: path.resolve(__dirname,'page.html'),
-                protocol: 'file:',
-                slashes: true
-            }));
-            win.webContents.once('did-finish-load', function () {
+            if(win === null){
+                win = new BrowserWindow({
+                    x: 0,
+                    y: 0,
+                    width: options.width,
+                    height: options.height,
+                    show: false,
+                    frame: false,
+                    enableLargerThanScreen: true
+                });
+                win.once('closed', function () {
+                    win = null;
+                });
+                win.loadURL(url.format({
+                    pathname: path.resolve(__dirname,'page.html'),
+                    protocol: 'file:',
+                    slashes: true
+                }));
+                win.webContents.on('did-finish-load', function () {
+                    win.webContents.send('svg', code, options.width, options.height);
+                });
+            }else{
+                win.setSize(options.width, options.height);
                 win.webContents.send('svg', code, options.width, options.height);
-            });
-            ipcMain.once('svg', function(event, string) {
+            }
+            ipcMain.on('svg', function(event, string) {
                 var regex = /^data:.+\/(.+);base64,(.*)$/;
                 var matches = string.match(regex);
                 var data = matches[2];
                 var buffer = new Buffer(data, 'base64');
-                win.close();
+                // win.close();
                 resolve(buffer);
             });
         };
 
-        if(svg.toLowerCase().indexOf('<svg') > -1){
+        if((svg.toLowerCase().indexOf('<svg') > -1) || (svg === "")){
             electronProcess(svg, options);
         }else{
             fs.lstat(svg, function (err, stats){
--- spec/javascripts/electron_fallback/SvgSpec.js
+++ spec/javascripts/electron_fallback/SvgSpec.js
@@ -5,27 +5,30 @@ describe("Electron fallback", function() {
     var filepath = os.tmpdir() + path.sep + 'temp.svg';
     var fs = require('fs');
     var svgCode = '<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="#fff"><g class="nc-icon-wrapper" fill="#444444"> <path d="M24 4C12.95 4 4 12.95 4 24s8.95 20 20 20 20-8.95 20-20S35.05 4 24 4zm-4 29V15l12 9-12 9z"></path> </g> </svg>';
-
+    beforeEach(function() {
+        originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 3000;
+    });
     it("Export is a function", function() {
         expect(typeof(instance)).toEqual('function');
     });
-    it("Fallback with undefined code", function () {
-        instance("", {width: 64, height: 64, fallback: true}, function (err, data) {
-            expect(typeof(err)).toEqual("object");
-            expect(typeof(data)).toEqual("object");
+    it("Fallback with undefined code", function (done) {
+        instance("", {width: 64, height: 64}).then(function (result) {
+            console.log(result);
+            done();
         });
     });
-    it("Fallback with svg code", function () {
-        instance(svgCode, {width: 64, height: 64, fallback: true}, function (err, data) {
-            expect(typeof(err)).toEqual("object");
-            expect(typeof(data)).toEqual("object");
+    it("Fallback with svg code", function (done) {
+        instance(svgCode, {width: 64, height: 64}).then(function(result){
+            console.log(result);
+            done();
         });
     });
-    it('Fallback with saved file', function () {
+    it('Fallback with saved file', function (done) {
        fs.writeFile(filepath, svgCode, function (fsErr) {
-           instance(filepath, {width: 64, height: 64, fallback: true}, function (err, data) {
-               expect(typeof(err)).toEqual("object");
-               expect(typeof(data)).toEqual("object");
+           instance(filepath, {width: 64, height: 64}).then(function (result) {
+                console.log(result);
+                done();
            });
        });
     });
